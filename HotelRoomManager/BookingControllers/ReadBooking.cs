3a0d4e787ca005b0a6bb1b1415de50954f0ee1fe
Frederick <frederick.wennborg@gmail.com>
2022-12-18 17:28:57 +0100

    #34 Finished formatting Read Bookings Column

--- HotelRoomManager/BookingControllers/ReadBooking.cs
+++ HotelRoomManager/BookingControllers/ReadBooking.cs
@@ -20,15 +20,19 @@ namespace HotelRoomManager.BookingControllers
 
         public bool ReadAllBookings()
         {
-            Console.WriteLine($"{Environment.NewLine}BokningsID\tFrån\t\tTill\t\tKund\t\tRum {Environment.NewLine}");
+            
+            Console.WriteLine("{0,-20} {1,-20} {2,-20} {3,-20}{4,-20}", $"{Environment.NewLine}BokningsID", "Från", "Till", "Namn",$"Rum {Environment.NewLine}");
             foreach (var booking in dbContext.Bookings
                          .Include(b => b.Customer)
-                         .ThenInclude(c=> c.Salutation)
+                         .ThenInclude(c => c.Salutation)
                          .Include(b => b.Room))
             {
-                Console.WriteLine($"{booking.Id}\t\t{booking.StartDate.ToShortDateString()}\t{booking.EndDate.ToShortDateString()} " +
-                                  $"\t{booking.Customer.Salutation.SalutationType}{booking.Customer.LastName}\t{booking.Room.Id}");
-                Console.WriteLine($"------------------------------------------------------------------------------");
+                Console.WriteLine("{0,-18} {1,-20} {2,-20} {3,-19} {4,-20}", 
+                    $"{booking.Id}", $"{booking.StartDate.ToShortDateString()}",
+                    $"{booking.EndDate.ToShortDateString()}", 
+                    $"{booking.Customer.Salutation.SalutationType}{booking.Customer.LastName}", 
+                    $"{booking.Room.Id}");
+                Console.WriteLine($"-----------------------------------------------------------------------------------------------------------");
             }
 
             if (dbContext.Bookings.Count() > 0)
